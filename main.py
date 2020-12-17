d2bd6c8f3b41648e569f1303966f2472f136d9c9
IsaacYantes23 <817296@mystma.org>
2020-12-17 16:47:58 +0000

    topfivemovie try 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def topFiveMovies(movieone, movietwo, moviethree, moviefour, moviefive):
-    return movieone + movietwo + moviethree + moviefour + moviefive
+def topFiveMovies(a, b, c, d, e):
+    return a, b, c, d, e
 
-print (topFiveMovies("Avengers", "Avengers Infinity War", "The Fast and the Furious", "Avengers Endgame", "Gran Torino"))
+print (topFiveMovies("Avengers",  "Avengers Infinity War",  "The Fast and the Furious",  "Avengers Endgame",  "Gran Torino"))
